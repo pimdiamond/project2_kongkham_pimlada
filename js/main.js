8e5aaf4919ec2ac55027dcdbe4b0b8e6d31252dd
Pimlada Kongkham <pkongkham@ufl.edu>
2021-03-14 16:49:40 -0400

    added js folder

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+mibreitGallery.createGallery({
+
+// slideshow container
+slideshowContainer:"#full-gallery",
+
+// thumb view container
+thumbviewContainer:".mibreit-thumbview",
+
+// image title container
+titleContainer:"#full-gallery-title",
+
+// determine whether to enable fullscreen mode
+allowFullscreen:!0,
+
+// image preloader
+preloadLeftNr:2,
+preloadRightNr:3,
+
+})
+})
